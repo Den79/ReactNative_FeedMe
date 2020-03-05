d8061f686361e4f880086b7e03d500b24c866c59
Den79 <test08081979@gmail.com>
2020-03-05 12:39:40 -0800

    Search field done. Need style

---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, View, TextInput } from "react-native";
+
+const SearchBAr = () => {
+  return (
+    <View>
+      <TextInput placeholder="search" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchBAr;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
+import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
   return (
     <View>
-      <Text>Search</Text>
+      <SearchBar />
     </View>
   );
 };
